be31331ff46f89a4ddbcb4d4667078eea2bf37ee
Oscar996 <72895074+Oscar996@users.noreply.github.com>
2021-10-14 14:17:39 -0400

    Add files via upload

---
+++ Instalacao DMView.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Instalacao do wget
+dnf install wget -y
+
+#Download do arquivo de instalacao MongoDB
+wget --no-check-certificate https://download.datacom.com.br/ftp/produtos/DmView/Database_MongoDB/Linux/Linux/64/mongodb-linux-x86_64-rhel70-3.4.13.tgz
+
+#Download do arquivo de instalacao Oracle
+wget --no-check-certificate https://download.datacom.com.br/ftp/produtos/DmView/Database_Oracle_Instaladores/oracle-xe-11.2.0-1.0.Linuxx86_64.rpm.zip
+
+
+#Download do arquivo de instalacao DMView
+wget --no-check-certificate https://download.datacom.com.br/ftp/produtos/DmView/DmView_Instaladores/DmView_11/Linux/010.0001.94-DmView-11.0.1-5-linux-20210805203535.jar
+
+
+#Instalacao do Oracle
+
+#Verificar hostname
+hostname
+cat /etc/hosts
+
+#Descompactacao do Oracle
+unzip oracle-xe-11.2.0-1.0.Linuxx86_64.rpm.zip
+cd Disk1
+
+#Execucao do Instalador do Oracle
+rpm -Uvh oracle-xe-11.2.0-1.0.x86_64.rpm
+
+
