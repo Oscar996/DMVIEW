50e5c951e4ec9a1c5b5001240b31331e38196fa7
Oscar996 <72895074+Oscar996@users.noreply.github.com>
2021-10-15 15:11:27 -0400

    Update Instalacao DMView.sh

--- Instalacao DMView.sh
+++ Instalacao DMView.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Download do arquivo de instalacao MongoDB
-curl -kLJO https://download.datacom.com.br/ftp/produtos/DmView/Database_MongoDB/Linux/Linux/64/mongodb-linux-x86_64-rhel70-3.4.13.tgz
+curl -kLJOv https://download.datacom.com.br/ftp/produtos/DmView/Database_MongoDB/Linux/Linux/64/mongodb-linux-x86_64-rhel70-3.4.13.tgz
 
 #Download do arquivo de instalacao Oracle
-curl -kLJO https://download.datacom.com.br/ftp/produtos/DmView/Database_Oracle_Instaladores/oracle-xe-11.2.0-1.0.Linuxx86_64.rpm.zip
+curl -kLJOv https://download.datacom.com.br/ftp/produtos/DmView/Database_Oracle_Instaladores/oracle-xe-11.2.0-1.0.Linuxx86_64.rpm.zip
 
 #Download do arquivo de instalacao DMView
-curl -kLJO https://download.datacom.com.br/ftp/produtos/DmView/DmView_Instaladores/DmView_11/Linux/010.0001.94-DmView-11.0.1-5-linux-20210805203535.jar
+curl -kLJOv https://download.datacom.com.br/ftp/produtos/DmView/DmView_Instaladores/DmView_11/Linux/010.0001.94-DmView-11.0.1-5-linux-20210805203535.jar
 
 #Instalacao do Oracle
 
