2fb10eeea02890d119450c1192ec1509745b1728
Oscar996 <72895074+Oscar996@users.noreply.github.com>
2021-10-22 11:00:31 -0400

    Update Instalacao DMView.sh

--- Instalacao DMView.sh
+++ Instalacao DMView.sh
@@ -30,11 +30,9 @@ cd Disk1
 rpm -Uvh oracle-xe-11.2.0-1.0.x86_64.rpm \
 && echo "Instalacao do Oracle concluida." || echo "Falha na instalacao do Oracle"
 
-
 #Configuracao Oracle
 printf '\8080\n1521\nnms\nnms\ny\n' | /etc/init.d/oracle-xe configure \
 && echo "Configuracao do Oracle concluida." || echo "Falha na configuracao do Oracle"
-
 
 #Configuracao do ulimit para o bom funcionamento do MongoDB
 ulimit -f unlimited
@@ -48,8 +46,8 @@ ulimit -a \
 && echo "Definicoes de sistema ajustadas para o bom funcionamento do MongoDB." || echo "Falha na configuracao do Ulimit"
 
 #Instalar pre-requisitos MongoDB
-yum install libcurl openssl xz-libs -y
-echo "Dependencias do MongoDB instaladas."
+yum install libcurl openssl xz-libs -y \
+&& echo "Dependencias do MongoDB instaladas" || echo "Falha no download de dependencias do MongoDB."
 
 #Copiar o arquivo para o diretorio opt e extrair o MongoDB
 cp mongodb-linux-x86_64-rhel70-3.4.13.tgz /opt
@@ -61,7 +59,8 @@ mv mongodb-linux-x86_64-rhel70-3.4.13 mongodb
 chmod -R 777 mongodb
 
 #Manter a variavel de ambiente atualizada
-echo 'export PATH=/opt/mongodb/bin:$PATH' >> /etc/bashrc
+echo 'export PATH=/opt/mongodb/bin:$PATH' >> /etc/bashrc \
+&& echo "Variavel de ambiente validada." || echo "Falha na validacao de variavel de ambiente."
 
 #Criacao de links simbolicos
 ln -s /opt/mongodb/bin/* /usr/bin
@@ -74,21 +73,28 @@ mongod
 echo "Instalacao MongoDB finalizada."
 
 #Instalacao dependencia DMView
-yum install java -y
-echo "Instalacao dependencias DmView concluida."
+yum install java -y \
+&& echo "Instalacao de dependencias do DMview concluida." || echo "Falha na instalacao de dependencias DmView"
 
 #*Verificar como realizar este procedimento
 #Inicializacao da instalacao do DMView
 Java –jar 010.0001.93-DmView-10.1.0-4-linux-20180808130157.jar –
-console
-echo "Instalacao do DmView concluida."
+console \
+&& echo "Instalacao e configuracao do DmView concluida." || echo "Falha no processo de instalacao do DMview"
 
 #Inicia os servicos do DMView
-/etc/init.d/nms.allservices start
+/etc/init.d/nms.allservices start \
+&& echo "DMview em funcionamento!!!" || echo "Falha ao iniciar servico DMview"
 
 #Configuracao de inicializacao automatica dos servicos
 cd /opt/DmView/bin/
 sudo ./auto_start_services.sh
 
 #Instalacao Client de acesso remoto DMView
-yum install -y xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps epel-release x2goserver
+yum install -y xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps epel-release x2goserver \
+&& echo "Instalacao de ferramentas de acesso remoto concluidas." || echo "Falha na download de ferramentas de acesso remoto"
+
+echo -e "Instalacao concluida!!! \n
+Acesse a Interface Web utilizando o IP do servidor atraves da porta :8101.\n
+Usuario e senha padrao: Adminstrator / administrator . \n
+Para acesso ao DMview, instale e configure o X2goClient na estacao de trabalho."
